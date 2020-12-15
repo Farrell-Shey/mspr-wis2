5bb0fa9bc51dc05b3b5c95b856184452e4f75b5c
Elliot <elliot.decle@gmail.com>
2020-12-15 09:57:36 +0100

    update : header / npm bootstrap-icons

--- elements/header.php
+++ elements/header.php
@@ -1,18 +1,41 @@
 <!doctype html>
-<html lang=fr>
+<html lang=fr-fr>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+    <title>WIP</title>
 </head>
 <body>
 <header>
-    <nav>
-        <ul>
-            <li>
-
+    <nav class="navbar navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Search
+                </a>
+            </li>
+            <li class="nav-item d-md-none">
+                <a class="nav-link" href="#">
+                    +
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    notif
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    profil
+                </a>
             </li>
         </ul>
     </nav>
